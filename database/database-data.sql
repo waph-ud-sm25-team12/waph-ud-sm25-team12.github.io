376aa236cf95a39dd90c3022da407705843c2c27
akhnoorn1 <akhnoorn1@udayton.edu>
2025-07-29 16:41:54 +0530

    database data created

---
+++ database/database-data.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    username VARCHAR(50) PRIMARY KEY,
+    password VARCHAR(100) NOT NULL
+);
+
+LOCK TABLES `users` WRITE;
+
+INSERT INTO users(username, password)
+VALUES ('Anvitha', MD5('Anvitha'));
+
+INSERT INTO users(username, password)
+VALUES ('Neeraj', MD5('Neeraj'));
+
+INSERT INTO users(username, password)
+VALUES ('Ryan', MD5('Ryan'));
+
+UNLOCK TABLES;
