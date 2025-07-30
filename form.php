1ffbd5529db890a4389af6de292d99357e82b8d7
4chengr <chengr4@udayton.edu>
2025-07-29 21:25:50 -0400

    improved form.php

--- form.php
+++ form.php
@@ -1,6 +1,36 @@
-<form action="index.php" method="POST" class="form login">
-Username :<input type="text" class="text_field" name="username" /> <br>
-Password: <input type="password" class="text_field" name="password" /> <br>
-<button class="button" type="submit">
-Login
-</button>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>WAPH_team_Login page</title>
+  <script type="text/javascript">
+      function displayTime() {
+        const options = {
+          month: 'short', // 'Jun'
+          day: '2-digit', // '24'
+          hour: '2-digit', // '07'
+          minute: '2-digit', // '03'
+          second: '2-digit', // '45'
+          hour12: true // 'am'
+        };
+        const formattedTime = new Date().toLocaleString('en-US', options).replace(/,/, '');
+        document.getElementById('digit-clock').innerHTML = "Current time: " + formattedTime;
+      }
+      setInterval(displayTime,500);
+  </script>
+</head>
+<body>
+  <h1>A Simple login form, WAPH_TEAM</h1>
+  <h2>Team12</h2>
+  <div id="digit-clock"></div>  
+<?php
+  //some code here
+  echo "Visited time: " . date("M-d h:i:sa")
+?>
+  <form action="index.php" method="POST" class="form login">
+    Username :<input type="text" class="text_field" name="username" /> <br>
+    Password: <input type="password" class="text_field" name="password" /> <br>
+    <button class="button" type="submit">Login</button>
+  </form>
+</body>
+</html>
